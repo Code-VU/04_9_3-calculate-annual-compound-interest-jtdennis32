6fdf564ec91fe299bfa417897e5f215e97ef2197
Jordan Dennis <Jordan.Dennis@vu.com>
2023-02-13 11:29:03 -0600

    compoundInterestJD

--- compoundInterest.py
+++ compoundInterest.py
@@ -1,10 +1,32 @@
 def calculateCompoundInterest():
     
 # This first 3 lines are provided for yougetACompoundInterest()
-# This first 3 lines are provided for you
- client_one_principal = float(input("Principle (amount): "))
- client_one_time =      float(input("Time:               "))
- client_one_rate =      float(input("Rate:               "))
+    def calculateCompoundInterest():
+        print("Compound Interest:", calculate_compound_interest())
+        print("---")
+
+        print("Compound Interest:", calculate_compound_interest())
+        print("---")
+
+        print("Compound Interest:", calculate_compound_interest())
+    
+
+    def calculate_compound_interest():
+        client_one_principal = float(input("Principle (amount): "))
+        client_one_time = float(input("Time:               "))
+        client_one_rate = float(input("Rate:               "))
+
+        amnt = client_one_principal*(1+client_one_rate/100)**client_one_time
+        compoundinterest = amnt - client_one_principal
+
+        return(round(compoundinterest,2))
+    
+    calculateCompoundInterest()
+    
+   
+        
+
+
  #print("Compound Interest: "+str(intrest))
 
     # end assignment
